591951e132e262f3d0411bcdeb95cfb86417cbb1
kangdokyeong <ehrud2701@naver.com>
2024-12-17 17:19:58 +0900

    첫 번째 업로드

---
+++ script.js
@@ -0,0 +1,5 @@
+// 버튼을 클릭하면 메시지가 나오는 기능
+const button = document.querySelector("button");
+button.addEventListener("click", function () {
+    alert("SWiet 프로젝트에 오신 것을 환영합니다! 더 많은 정보가 곧 준비됩니다.");
+});
